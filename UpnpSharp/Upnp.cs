f892deeddc3567884c8359265a4177bd49b472c0
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-08-06 21:20:23 +0800

    添加项目文件。

---
+++ UpnpSharp/Upnp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpnpSharp
+{
+    internal class Upnp
+    {
+        /// <summary>
+        /// 查找 UPnP 设备
+        /// </summary>
+        /// <param name="delay"></param>
+        /// <returns></returns>
+        public IEnumerable<UpnpDevice> Discover(int delay = 1)
+        {
+
+        }
+    }
+}
